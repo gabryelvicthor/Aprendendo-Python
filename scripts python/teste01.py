f124dcf484e327214cec97ce53df81a7ebe35586
Gabryel Victhor Cardoso dos Santos <gvicthorc@gmail.com>
2022-01-10 11:38:32 -0300

    alteração teste01

--- scripts python/teste01.py
+++ scripts python/teste01.py
@@ -1,6 +1,6 @@
-nome = input ('Qual é o seu nome ')
-idade = input ('Qual é a sua idade ')
-peso = input('Qual é o seu peso ')
+nome = input ('Qual é o seu nome: ')
+idade = input ('Qual é a sua idade: ')
+peso = input('Qual é o seu peso: ')
 
 print('Seu peso é: ', peso)
 print('Sua idade é: ', idade)
