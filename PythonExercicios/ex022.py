96d9fa20691b5b6ec1adb62699cfe5e3d3827ff9
Gabryel Victhor Cardoso dos Santos <gvicthorc@gmail.com>
2022-01-17 11:16:10 -0300

    antes do almoço

--- PythonExercicios/ex022.py
+++ PythonExercicios/ex022.py
@@ -1,4 +1,4 @@
-nome = input('Digite seu nome completo: ')
+nome = input('Digite seu nome completo: ')#.strip()
 
 print('Nome com letras maiúsculas: {}'.format(nome.upper()))
 print('Nome com letras minúsculas: {}'.format(nome.lower()))
@@ -7,3 +7,6 @@ nomeModif = nome.split()
 nomeJunto = ''.join(nomeModif)
 print('Quantidade de letras: {}'.format(len(nomeJunto)))
 print('Letras no primeiro nome: {}'.format(len(nomeModif[0])))
+
+# print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+# print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
---
+++ PythonExercicios/ex023.py
@@ -0,0 +1,18 @@
+num = int(input('Informe um número: '))
+print('Analisando o número {}'.format(num))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhar: {}'.format(m))
+
+# num = input('Informe um número: ')
+# print('Analisando o número {}'.format(num))
+# print('Unidade: {}'.format(num[0]))
+# print('Dezena: {}'.format(num[0]))
+# print('Centena: {}'.format(num[1]))
+# print('Milhar: {}'.format(num[0]))
