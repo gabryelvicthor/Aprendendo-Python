7199d95576a90fe6e283c600dc513b0604a1c39f
Gabryel Victhor Cardoso dos Santos <gvicthorc@gmail.com>
2023-08-28 21:17:46 -0300

    Update Testes.py

--- PythonExercicios/Python para iniciantes DATAB/Testes.py
+++ PythonExercicios/Python para iniciantes DATAB/Testes.py
@@ -2,4 +2,8 @@ tipo1 = 10
 tipo2 = "bola"
 tipo3 = True
 
-print("Tipo1: {}, Tipo2: {}, Tipo3: {}".format(type(tipo1), type(tipo2), type(tipo3)))
+#print("Tipo1: {}, Tipo2: {}, Tipo3: {}".format(type(tipo1), type(tipo2), type(tipo3)))
+
+
+print(f"Texto1")
+print(f"Texto2")
